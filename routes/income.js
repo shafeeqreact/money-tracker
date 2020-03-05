601b210a6c36301b45abbd9fa96dadce87925ba7
shafeeqreact <47767599+shafeeqreact@users.noreply.github.com>
2020-03-05 16:40:41 -0500

    Update income.js

--- routes/income.js
+++ routes/income.js
@@ -1,48 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const { ObjectId } = require('mongodb');
-
-const connectDB = require('../db/connectDB');
+const { getAllTransactions, getOneTransaction, insertOneTransaction, deleteOneTransaction } = require('../db/connectDB');
 
 router.route('/')
     .get(async (req, res) => {
         try {
-            const task = async (collection) => await collection.find().toArray();
-            const resp = await connectDB(task);
+            const resp = await getAllTransactions();
             res.status(200).send(resp);
         } catch (err) {
             res.status(400).send({ success: false, error: err });
         }
     })
     .post(async (req, res) => {
-        const document = {
-            date: req.body.date,
-            earnings: {
-                basic: req.body.earnings.basic,
-                hra: req.body.earnings.hra,
-                conveyanceReimbursement: req.body.earnings.conveyanceReimbursement,
-                adhoc: req.body.earnings.adhoc,
-                transportAllowance: req.body.earnings.transportAllowance,
-                ltaTaxable: req.body.earnings.ltaTaxable,
-                medicalTaxable: req.body.earnings.medicalTaxable,
-                odcBonus: req.body.earnings.odcBonus
-            },
-            deductions: {
-                providentFund: req.body.deductions.providentFund,
-                professionalTax: req.body.deductions.professionalTax,
-                welfareFund: req.body.deductions.welfareFund
-            },
-            totalEarnings: req.body.totalEarnings,
-            totalDeductions: req.body.totalDeductions,
-            netPay: req.body.netPay,
-            createdAt: new Date(),
-            lastUpdatedAt: new Date()
-        }
-
         try {
-            const task = async (collection) => await collection.insertOne(document);
-            const resp = await connectDB(task);
+            const resp = await insertOneTransaction(req.body);
             res.status(200).send(resp);
         } catch (err) {
             res.status(400).send({ success: false, error: err });
@@ -50,16 +22,20 @@ router.route('/')
     })
 
 router.route('/:id')
-    .get((req, res) => {
-        res.status(200).send('From the get with id income router')
+    .get(async (req, res) => {
+        try {
+            const resp = await getOneTransaction(req.params.id);
+            res.status(200).send(resp);
+        } catch (err) {
+            res.status(400).send({ success: false, error: err });
+        }
     })
     .put((req, res) => {
         res.status(200).send('From the put with id income router')
     })
     .delete(async (req, res) => {
         try {
-            const task = async (collection) => await collection.deleteOne({ _id: ObjectId(req.params.id) });
-            const resp = await connectDB(task);
+            const resp = await deleteOneTransaction(req.params.id);
             res.status(200).send(resp);
         } catch (err) {
             res.status(400).send({ success: false, error: err });
